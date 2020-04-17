54bd3555fff146a7ae3b7e5e74493ed280d2e9e6
meizhiying <meizhiying@genomics.cn>
2020-04-17 15:16:56 +0800

    SARS-CoV-2 pipeline v1.0

--- bin/Main_SARS-CoV-2.py
+++ bin/Main_SARS-CoV-2.py
@@ -76,16 +76,17 @@ def bwaaln(script,barcode,fqtype,read_len):
 	Align_dir = barcode_dir + '/02.Align'
 	create_dirs(Align_dir)
 	seed_len = str(int(int(read_len)*0.9))
+	max_diff_seed = int(int(seed_len)/16)
 	if fqtype == 'SE':
 		cleanfq = Clean_dir + '/Clean_' + barcode + '.fq.gz'
-		script.write("%(bwa)s aln -l %(seed_len)s -t 3 -f %(Align_dir)s/%(barcode)s.sai %(database)s/nCoV.fa %(cleanfq)s && %(bwa)s samse -r \"@RG\\tID:PE100\\tPL:MGISEQ\\tPU:PE100\\tLB:mutPCR\\tSM:%(barcode)s\\tCN:BGI\" %(database)s/nCoV.fa %(Align_dir)s/%(barcode)s.sai %(cleanfq)s | %(samtools)s view -b - | %(samtools)s sort -T %(Align_dir)s/%(barcode)s.sort -o %(Align_dir)s/%(barcode)s.sort.bam - \n" \
-			%{'bwa':bwa,'samtools':samtools,'database':database,'cleanfq':cleanfq,'Align_dir':Align_dir,'barcode':barcode,'seed_len':seed_len})
+		script.write("%(bwa)s aln -l %(seed_len)s -k %(max_diff_seed)s -t 3 -f %(Align_dir)s/%(barcode)s.sai %(database)s/nCoV.fa %(cleanfq)s && %(bwa)s samse -r \"@RG\\tID:PE100\\tPL:MGISEQ\\tPU:PE100\\tLB:mutPCR\\tSM:%(barcode)s\\tCN:BGI\" %(database)s/nCoV.fa %(Align_dir)s/%(barcode)s.sai %(cleanfq)s | %(samtools)s view -b - | %(samtools)s sort -T %(Align_dir)s/%(barcode)s.sort -o %(Align_dir)s/%(barcode)s.sort.bam - && rm %(Align_dir)s/%(barcode)s.sai\n" \
+			%{'bwa':bwa,'samtools':samtools,'database':database,'cleanfq':cleanfq,'Align_dir':Align_dir,'barcode':barcode,'seed_len':seed_len,'max_diff_seed':max_diff_seed})
 		script.write("%(samtools)s index %(Align_dir)s/%(barcode)s.sort.bam \n" %{'samtools':samtools,'Align_dir':Align_dir,'barcode':barcode})
 	elif fqtype == 'PE':
 		cleanfq1 = Clean_dir + '/Clean_' + barcode + '_1.fq.gz'
 		cleanfq2 = Clean_dir + '/Clean_' + barcode + '_2.fq.gz'
-		script.write("%(bwa)s aln -l %(seed_len)s -t 3 -f %(Align_dir)s/%(barcode)s_1.sai %(database)s/nCoV.fa %(cleanfq1)s && %(bwa)s aln -l %(seed_len)s -t 3 -f %(Align_dir)s/%(barcode)s_2.sai %(database)s/nCoV.fa %(cleanfq2)s && %(bwa)s sampe -a 1000 -r \"@RG\\tID:PE100\\tPL:MGISEQ\\tPU:PE100\\tLB:mutPCR\\tSM:%(barcode)s\\tCN:BGI\" %(database)s/nCoV.fa %(Align_dir)s/%(barcode)s_1.sai %(Align_dir)s/%(barcode)s_2.sai %(cleanfq1)s %(cleanfq2)s | %(samtools)s view -b - | %(samtools)s sort -T %(Align_dir)s/%(barcode)s.sort -o %(Align_dir)s/%(barcode)s.sort.bam - \n" \
-			%{'bwa':bwa,'samtools':samtools,'database':database,'cleanfq1':cleanfq1,'cleanfq2':cleanfq2,'Align_dir':Align_dir,'barcode':barcode,'seed_len':seed_len})
+		script.write("%(bwa)s aln -l %(seed_len)s -k %(max_diff_seed)s -t 3 -f %(Align_dir)s/%(barcode)s_1.sai %(database)s/nCoV.fa %(cleanfq1)s && %(bwa)s aln -l %(seed_len)s -k %(max_diff_seed)s -t 3 -f %(Align_dir)s/%(barcode)s_2.sai %(database)s/nCoV.fa %(cleanfq2)s && %(bwa)s sampe -a 1000 -r \"@RG\\tID:PE100\\tPL:MGISEQ\\tPU:PE100\\tLB:mutPCR\\tSM:%(barcode)s\\tCN:BGI\" %(database)s/nCoV.fa %(Align_dir)s/%(barcode)s_1.sai %(Align_dir)s/%(barcode)s_2.sai %(cleanfq1)s %(cleanfq2)s | %(samtools)s view -b - | %(samtools)s sort -T %(Align_dir)s/%(barcode)s.sort -o %(Align_dir)s/%(barcode)s.sort.bam - && rm %(Align_dir)s/%(barcode)s_1.sai %(Align_dir)s/%(barcode)s_2.sai\n" \
+			%{'bwa':bwa,'samtools':samtools,'database':database,'cleanfq1':cleanfq1,'cleanfq2':cleanfq2,'Align_dir':Align_dir,'barcode':barcode,'seed_len':seed_len,'max_diff_seed':max_diff_seed})
 		script.write("%(samtools)s index %(Align_dir)s/%(barcode)s.sort.bam \n" %{'samtools':samtools,'Align_dir':Align_dir,'barcode':barcode,'seed_len':seed_len})
 	return
 
